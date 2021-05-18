52e00e2154e314feddbf437293c1db3f732f83f6
Elemigrante <gasdom@yandex.ru>
2021-05-18 04:40:30 -0400

    Added devise trackable feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+
   resources :courses
+  resources :users, only: %i[index]
+
   root 'static_pages#landing_page'
 
   get 'privacy_policy', to: 'static_pages#privacy_policy'
